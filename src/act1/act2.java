fcd89b806b08c1277c14c0fbf6268363cfe52f23
shusaaitano <shusaaitano@gmail.com>
2025-07-31 12:56:56 +0800

    rodriguez activity

---
+++ src/act1/act2.java
@@ -0,0 +1,33 @@
+
+package act1;
+
+import java.util.Scanner;
+
+
+public class act2 {
+     public static void main(String[] args) {
+        
+     Scanner sc = new Scanner(System.in);   
+        
+   int num;
+   
+   do{
+   System.out.print("Enter a number: ");
+   num = sc.nextInt();
+   
+   
+   if (num ==0){
+       System.out.println("Inputted number Zero");
+   }else if (num %2 != 0){
+       System.out.println("Inputted number is Odd");
+   }else{
+      System.out.println("Inputted number is Even");
+       
+       
+   }
+   
+   }while(num !=0);
+   
+  System.out.println("End");
+    }
+}
---
+++ src/act1/act3.java
@@ -0,0 +1,34 @@
+
+package act1;
+
+import java.util.Scanner;
+
+
+public class act3 {
+     public static void main(String[] args) {
+        
+     Scanner sc = new Scanner(System.in);   
+        
+   int num;
+   
+   do{
+   System.out.print("Enter a number: ");
+   num = sc.nextInt();
+   
+   
+   if (num > 0){
+       System.out.println(num+" is a positive number");
+   }else if (num < 0){
+       System.out.println(num+" is a negative number");
+   }else{
+      System.out.println("Inputted number zero");
+       
+       
+   }
+   
+   }while(num !=0);
+   
+  System.out.println("End");
+    }
+}
+
