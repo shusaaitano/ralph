26f60c9a5679612bf832c4da7f753d81b69c3749
shusaaitano <shusaaitano@gmail.com>
2025-08-11 14:02:01 +0800

    added panel

---
+++ src/bankapp/main.java
@@ -0,0 +1,100 @@
+
+package bankapp;
+
+import bankclass.banking;
+import java.util.Scanner;
+
+ 
+public class main {
+     public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+
+        System.out.println("HELLO WELCOME TO THE SYSTEM!");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+           
+       banking bs[] = new banking[10];
+       int index = 0;
+            System.out.println("Enter Choice: ");
+            int choice = sr.nextInt();
+            
+            switch(choice){
+                case 1:
+                    int resp;
+                     do {
+                System.out.println("1. Register Account");
+                System.out.println("2. Login Account");
+                System.out.println("3. View All Accounts");
+                System.out.println("Enter Selection: ");
+                int action = sr.nextInt();
+           
+                switch(action){
+                    case 1:
+                        if(index >= bs.length){
+                            System.out.println("Registration accounts reached");
+                            break;
+                        }
+                        
+                        bs[index] = new banking();
+                        System.out.println("Enter Account No: ");
+                        bs[index].setAccountNo(sr.nextInt());
+                        
+                        System.out.println("Enter Pin: ");
+                        bs[index].setPin(sr.nextInt());
+                        
+                        System.out.println("Registration Successful");
+                        index++;
+                        break;
+                        
+                    case 2:
+                        int attempts = 3;
+                        boolean found = false;
+                        
+                        while(attempts > 0){
+                            System.out.println("Enter Account No: ");
+                            int accn = sr.nextInt();
+                            
+                            System.out.println("Enter Pin: ");
+                            int pi = sr.nextInt();
+                            
+                            for(int acc = 0; acc < index; acc++){
+                            if(bs[acc].validateAccount(accn, pi)){
+                                System.out.println("LOGIN SUCCESSFUL");
+                                found = true;
+                                break;
+                                  } 
+                            }
+                            if(found){
+                                break;
+                            }else{
+                                attempts--;
+                                System.out.println("LOGIN FAILED!");
+                                System.out.println(attempts+" Attempts Remaining");
+                                
+                               if (attempts == 0){
+                                    System.out.println("Max Attempts Reached");
+                                    System.out.println("Access denied");
+                               }
+                            }
+                        }
+                        break;
+                    case 3:
+                        break;
+                }
+                        System.out.println("Do you want to continue? (Y = 1/N = 0)");
+                        resp = sr.nextInt();
+                }while(resp == 1);
+                  break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid Selection!");
+                    System.out.println("Try again.");
+            }
+        }
+      }          
+
---
+++ src/bankclass/banking.java
@@ -0,0 +1,58 @@
+
+package bankclass;
+
+
+public class banking {
+    
+        private int accountNo, pin;
+    private float initializeBalance;
+    
+    
+    public void setAccountNo(int accountNo){
+        this.accountNo = accountNo;
+    }
+    
+    public int getAccountNo(){
+        return accountNo;
+    }
+    
+    public void setPin(int pin){
+        this.pin = pin;
+    }
+    
+    public int getPin(){
+        return pin;
+    }
+   
+    public boolean validateAccount(int accn, int pi){
+        
+        if(accn == accountNo && pi == pin){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+ 
+    public int listAccount(){
+        return accountNo;
+    }
+    
+    
+    
+    
+    
+    public void viewBalance(){
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+
+}
